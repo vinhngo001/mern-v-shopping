74f12dce6e7f342273943d983aab16fb453523f2
DESKTOP-3VG7I1R\Vinh Ngo <vinhtrungngo1907@gmail.com>
2023-01-21 21:28:11 +0700

    style css

--- client/src/components/ActionBtn.jsx
+++ client/src/components/ActionBtn.jsx
@@ -8,28 +8,28 @@ const BtnRender = ({ product, deleteProduct }) => {
     const addCart = state.userAPI.addCart;
 
     return (
-        <>
+        <div className='purchase-info'>
             {
                 isAdmin
                     ? <>
                         <Link id="btn_buy" to="#!"
-                            onClick={() => deleteProduct(product._id, product.images.public_id)}>
+                            onClick={() => deleteProduct(product._id, product.images.public_id)} className="btn btn_buy">
                             Delete
                         </Link>
-                        <Link id="btn_view" to={`/product/edit/${product._id}`}>
+                        <Link id="btn_view" to={`/product/edit/${product._id}`} className="btn btn_view">
                             Edit
                         </Link>
                     </>
                     : <>
-                        <Link id="btn_buy" to="#!" onClick={() => addCart(product)}>
+                        <Link id="btn_buy" to="#!" onClick={() => addCart(product)} className="btn btn_buy">
                             Buy
                         </Link>
-                        <Link id="btn_view" to={`/product/detail/${product._id}`}>
+                        <Link id="btn_view" to={`/product/detail/${product._id}`} className="btn btn_view">
                             View
                         </Link>
                     </>
             }
-        </>
+        </div>
     )
 }
 
